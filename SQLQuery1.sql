d74cc46478ac8b187f6b9b21df69787e9f7fd6a7
Sriram R <sriram_tpr@hotmail.com>
2021-06-22 19:56:39 +1200

    Question A Answered.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,11 @@
-﻿SELECT TOP (1000) [Id]
-  FROM [Keys].[dbo].[Owners]
+﻿--Display a list of all property names and their property id’s for Owner Id: 1426
+SELECT 
+	p.Name, 
+	p.Id
+FROM
+[Property] as p
+left join [OwnerProperty] as o
+ON p.Id = o.PropertyId
+WHERE o.OwnerId = '1426'
+
+
